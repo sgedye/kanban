75f93db23f9b4eca4c1ac171b437d61b26724d7f
Shaun Gedye <sgedye88@yahoo.com.au>
2022-05-05 20:27:02 +0800

    Config - Use React18 render method

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,15 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 
 import { App } from "./App";
 
 import "./assets/styles/main.scss";
 
-ReactDOM.render(
+const container = document.getElementById("root");
+const root = createRoot(container!);
+
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
